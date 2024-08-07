f6db2abe3975a75e8328821d75eb2ff94b706601
Igor Laptop <rogit31@gmail.com>
2024-08-07 02:49:09 -0700

    Fixed logout method

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -59,6 +59,7 @@ class AuthController{
     }
     public function logout(){
         session_start();
+        session_unset();
         session_destroy();
         header('Location: /home');
     }
